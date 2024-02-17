f1852acab23aeb210308acdccba1cc7063a2ffbc
Anut T <135195724+AnutToc@users.noreply.github.com>
2024-02-17 18:23:34 +0700

    Update __manifest__.py

--- personal_data/__manifest__.py
+++ personal_data/__manifest__.py
@@ -5,8 +5,8 @@
     "depends": ["base", "report_xlsx"],
     "data": [
         "security/ir.model.access.csv",
-        "views/personal_data_menus.xml",
         "views/personal_data_views.xml",
+        "views/personal_data_menus.xml",
         "data/report_paperformat.xml",
         "report/personal_data_templates.xml",
         "report/personal_data_reports.xml",
